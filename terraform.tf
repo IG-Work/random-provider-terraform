416c747d7b4af8baa02d11bb7abd67dbc89b6c8e
Yashi Chauhan <64953869+yashi-coder@users.noreply.github.com>
2025-06-21 11:37:41 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -11,17 +11,20 @@ provider "random" {}
 
 resource "random_id" "example" {
   byte_length = 8
+  count = 10
 }
 
 resource "random_password" "password" {
   length           = 16
   special          = true
   override_special = "!#$%&*()-_=+[]{}<>:?"
+  count = 10
 }
 
 resource "random_pet" "example" {
   length    = 2
   separator = "-"
+  count = 10
 }
 
 output "random_id_example" {
